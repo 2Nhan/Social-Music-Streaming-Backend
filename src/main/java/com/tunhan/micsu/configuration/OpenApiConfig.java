df63cda102312b283bf10d7498ce92b216f60e1d
2Nhan <letunhan137@gmail.com>
2026-04-01 18:11:45 +0700

    feat(apidocs): commit api docs v1

---
+++ src/main/java/com/tunhan/micsu/configuration/OpenApiConfig.java
@@ -0,0 +1,38 @@
+package com.tunhan.micsu.configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Micsu - Music Streaming API")
+                        .version("1.0.0")
+                        .description("RESTful API documentation for the Micsu music streaming platform.")
+                        .contact(new Contact()
+                                .name("TunHan")))
+                .servers(List.of(
+                        new Server().url("https://pj2.crmsoft.site").description("Production Server"),
+                        new Server().url("http://localhost:8080").description("Local Server")))
+                .addSecurityItem(new SecurityRequirement().addList("BearerAuth"))
+                .components(new Components()
+                        .addSecuritySchemes("BearerAuth", new SecurityScheme()
+                                .name("BearerAuth")
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
+    }
+}
